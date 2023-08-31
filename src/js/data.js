a506836f5ff53a8bda1a797894c542b41c923548
Fullmetal Catgirl <johnsmithsemail81@gmail.com>
2023-08-31 11:13:38 +1000

    test for website

--- src/js/data.js
+++ src/js/data.js
@@ -13,7 +13,7 @@ const dataSet = {};
 /** 
  * Data set version, in YYYY-MM-DD form.
  * 
- * @example '2018-02-20'
+ * @example '2023-08-31'
 */
 let dataSetVersion = '';
 
--- src/js/data/2023-08-31.js
+++ src/js/data/2023-08-31.js
@@ -1,2 +1,57 @@
 dataSetVersion = "2023-08-31"
 dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = [
+    {
+        name: "Filter by Game",
+        key: "game",
+        tooltip: "Click this to restrict to certain games",
+        checked: true,
+        sub: [
+            { name: "Aokana - Four Rhythms Across the Blue", tooltip: "青の彼方のフォーリズム", key: "aokana"}
+        ]
+    },
+    {
+        name: "Filter by Role",
+        key: "role",
+        tooltip: "Click this to restrict characters based on their role in game",
+        checked: true,
+        sub: [
+            { name: "Main Heroines", tooltip: "Characters with a main route", key: "main"},
+            { name: "Side Heroines", tooltip: "Characters with a side route", key: "side"},
+            { name: "Side Characters", tooltip: "Characters without a route", key: "noroute"},
+            { name: "Protagonists", tooltip: "The protagonists of the game", key: "protag"},
+        ]
+    },
+    {
+        name: "Remove Male Characters",
+        key: "male",
+        tooltip: "Click this to remove male characters from the list"
+    },
+    {
+        name: "Remove Female Characters",
+        key: "girl",
+        tooltip: "Click this to remove female characters from the list"
+    }
+];
+
+dataSet[dataSetVersion].characterData = [
+    {
+        name: "Hinata Masaya",
+        img: "5FurVqL.png",
+        opts: {
+            game: ["aokana"],
+            role: ["protag"],
+            male: true
+        }
+    },
+    {
+        name: "Arisaka Mashiro",
+        img: "rbYidTN.png",
+        opts: {
+            game: ["aokana"],
+            role: ["main"],
+            girl: true
+        }
+    }
+]
